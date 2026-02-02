6c01997c39c8833fb8495f174d8c748bfef6c0b6
federicosimone <federico.simone15@gmail.com>
2026-02-03 00:28:00 +0100

    imbastito footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import style from './Footer.module.css'
+
+function Footer() {
+    return (
+        <>
+            <div className={`${style.footer}`}>
+                Footer
+            </div>
+        </>
+    )
+}
+export default Footer
