7b0435e12280c369eb851ed9a060014cb9900e36
federicosimone <federico.simone15@gmail.com>
2026-02-02 21:17:52 +0100

    link to

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Products from './pages/Products'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,50 @@
 import axios from "axios";
+import { useState, useEffect } from 'react'
+import { Link } from "react-router-dom";
+
 
 function Products() {
+
+    const [products, setProdutc] = useState([])
+
+    useEffect(() => {
+        axios.get("https://fakestoreapi.com/products").then(res => {
+            console.log(res.data);
+            setProdutc(res.data)
+        }
+        )
+    }, []);
+
+
+
+
+
     return (
         <>
+            <div className="container mt-5">
+                <div className="d-flex flex-wrap gap-4">
+
+                    {products.map((product) => (
+
+                        <div id={product.id} className="card" style={{ width: "12rem" }} >
+                            <img src={product.image} className="card-img-top" alt="thumb" />
+                            <div className="card-body">
+                                <h5 className="card-title">{product.title}</h5>
+                                <p className="card-text">{product.description}</p>
+                                <Link className="btn btn-info" to="/products/1">Visualizza scheda prodotto</Link>
+                            </div>
+
+                        </div>
+
+
+                    )
+
+
+
+
+                    )}
+                </div>
+            </div>
         </>
     )
 }
